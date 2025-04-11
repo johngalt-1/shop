fc8826c18f55bd0a589cf12f36d92198912d5a94
Ilya <ilya>
2025-04-11 17:58:08 +0200

    Application

---
+++ src/main/java/ru/yandex/praktikum/shop/PraktikumShopApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.praktikum.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikumShopApplication.class, args);
+	}
+
+}
